41e293f97ad4d880fa78b9eee892c05560d6be8b
Javier Bermúdez <javier.bermudez@wanna.es>
2016-05-12 12:37:57 +0200

    Desde Wanna 2

--- app/server/server.js
+++ app/server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var data = require('./styleData.json');
 var simulated_stats = require('./simulated_stats.json');
+var ajax = require('superagent');
 
 app.set('view engine', 'ejs');
 app.use('/', express.static('public'));
@@ -9,11 +10,27 @@ app.use('/', express.static('public'));
 app.get('/', function (req, res) {
   console.log('GET --> /');
   res.render('index', { styleData: JSON.stringify(data) });
-})
+});
 
 app.get('/stats', function (req, res) {
   console.log('GET --> /stats');
   res.json(simulated_stats);
+});
+
+app.get('/stats/:server/:summonername/:apikey', function (req, res) {
+  console.log('GET --> /stats');
+
+  var url = 'https://'+req.params.server+'.api.pvp.net/api/lol/'+req.params.server+'/v1.4/summoner/by-name/'+req.params.summonername+'?api_key='+req.params.apikey;
+
+  ajax
+    .get(url)
+    .set('Accept', 'application/json')
+    .set('Content-type', 'application/json')
+    .end(function (err, data) {
+      console.log(data.body);
+      res.json(data.body);
+    });
+
 })
 
 var server = app.listen(2000, function () {
